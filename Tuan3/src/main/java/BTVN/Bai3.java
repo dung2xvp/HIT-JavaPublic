d02cd36de7dced650bf5f086434ef14c0101a3b9
dung2xvp <dung2xvp@gmail.com>
2024-08-03 11:30:07 +0700

    Update code

--- Tuan3/src/main/java/BTVN/Bai3.java
+++ Tuan3/src/main/java/BTVN/Bai3.java
@@ -12,11 +12,15 @@ public class Bai3 {
         a = rand.nextInt(11);
         b = rand.nextInt(11);
         c = rand.nextInt(21);
+        boolean per = Math.random() < 0.51;
+        if (!per) {
+            c = a + b;
+        }
         System.out.println(a + " + " + b + " = " + c);
         Scanner ip = new Scanner(System.in);
         System.out.print("Phep tinh tren dung hay sai(Y/N): ");
         String answer = ip.nextLine();
-        if (answer.equals("Y")) {
+        if (answer.equalsIgnoreCase("Y")) {
             if (a + b == c) {
                 System.out.println("Chinh xac!");
             }
@@ -24,7 +28,7 @@ public class Bai3 {
                 System.out.println("Sai!");
             }
         }
-        else if (answer.equals("N")) {
+        else if (answer.equalsIgnoreCase("N")) {
             if (a + b == c) {
                 System.out.println("Sai!");
             }
